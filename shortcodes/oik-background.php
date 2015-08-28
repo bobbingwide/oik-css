da0e37a4ca398809bd20726b1fe246e3192d6a8f
Herb Miller <herb@bobbingwide.com>
2015-08-28 17:03:32 +0100

    bw_background - added some comments and minor code change
    The [bw_background] shortcode is still experimental.
    
    - Added some docblock comments
    - Default selector is now 'body'
    - Added syntax for selector= parameter

--- shortcodes/oik-background.php
+++ shortcodes/oik-background.php
@@ -1,19 +1,31 @@
-<?php // (C) Copyright Bobbing Wide 2014
+<?php // (C) Copyright Bobbing Wide 2014,2015
 
 /**
  * Return the default selector for the theme's background image
  * 
  * In the first prototype we were probably doing this all wrong...
+ * 
  * By default, the [bw_background] shortcode now applies the background to the body
  * Using the selector parameter you can override where the background image is to be targeted
- * Note: Some themes display the featured image prominently. 
- * 
+ * Note: Some themes display the featured image prominently so using it as a background may not look right at all
+ *
+ * Default selector       | Theme  | area selected
+ * ---------------------- | ------ | -----------------
+ * article .entry-content | Twenty Twelve | article content - not the title
+ * article.art-article   | Artisteer 4.1 | article whole area
+ * article.hentry .art-postcontent | Artisteer 4.1 | article content, not the title 
+ * body | Genesis framework? | article content, not the title
  * 
  * @return string - the CSS selector to target
+ *
+ * @TODO This code is wrong - it only produces the right result for Artisteer 4.1 themes. Extend for other themes based on table above
+ *
  */
 function bw_default_selector_for_theme() {
-  $selector = apply_filters( "bw_default_selector_for_theme", "article .entry-content" );
-  $selector = apply_filters( "bw_default_selector_for_theme", "body.custom-background" );
+  //$selector = apply_filters( "bw_default_selector_for_theme", "article .entry-content" );
+  //$selector = apply_filters( "bw_default_selector_for_theme", "article.art-article" );
+  //$selector = apply_filters( "bw_default_selector_for_theme", "article.hentry .art-postcontent" );
+  $selector = apply_filters( "bw_default_selector_for_theme", "body" );
   return( $selector );
 } 
 
@@ -46,12 +58,16 @@ function bw_default_selector_for_theme() {
 function bw_background( $atts=null, $content=null, $tag=null ) {
   $atts['post_type'] = bw_array_get( $atts, "post_type", "attachment" );
   $atts['post_mime_type'] = bw_array_get( $atts, 'post_mime_type', 'image' );
-  $atts['id'] = bw_array_get_from( $atts, "id,0", bw_current_post_id() );
+  $atts['id'] = bw_array_get_from( $atts, "id,0", null );
+  if ( $atts['id'] === null ) {
+    // Don't set the ID yet.. let get_thumbnail_src do it.
+  }
   $atts['thumbnail'] = bw_array_get( $atts, "thumbnail", "full" );
   oik_require( "includes/bw_posts.inc" );
   $thumbnail = bw_get_thumbnail_size( $atts );
   //$posts = bw_get_posts();
   //foreach ( $posts as $post ) {
+  bw_trace2( $thumbnail, "thumbnail" );
     list( $thumbnail, $width, $height ) = bw_get_thumbnail_src( $atts['id'], $thumbnail, $atts );
     if ( $thumbnail ) {
       $selector = bw_array_get_from( $atts, "selector,1", null );
@@ -59,10 +75,12 @@ function bw_background( $atts=null, $content=null, $tag=null ) {
         $selector = bw_default_selector_for_theme();
       } 
       stag( "style", null, null, kv( "type", "text/css" ) . kv( "media", "screen,print")  );
-      $css = "$selector { background: url( $thumbnail ) no-repeat;  background-size: 100% auto; background-origin: padding-box; }";
+      $css = "$selector { background: url( \"$thumbnail\" ) no-repeat;  background-size: 100% auto; background-origin: padding-box; }";
       e( $css );
       etag( "style" );
       
+    } else {
+      p( "No thumbnail" );
     }
   //}
   return( bw_ret() );
@@ -79,12 +97,13 @@ function bw_background__help( $shortcode="bw_background" ) {
  * Syntax hook for [bw_background] shortcode
  */
 function bw_background__syntax( $shortcode="bw_background" ) {
-  $syntax = array( "id,0" => bw_skv( "current ID", "<i>ID</i>", "Post ID to find image to display" )
-                 , "thumbnail" => bw_skv( "full", "thumbnail|medium|large", "Image size to use" )
-                 , "post_type" => bw_skv( "attachment", "<i>post type</i>", "Post type" )
-                 , "post_mime_type" => bw_skv( "image", "<i>post mime type</i>", "Post mime type" )
-                 );
-  return( $syntax );
+	$syntax = array( "id,0" => bw_skv( "current ID", "<i>ID</i>", "Post ID to find image to display" )
+						, "selector,1" => bw_skv( "body", "<i>CSS selector</i>", "CSS selector for the background image" ) 
+						, "thumbnail" => bw_skv( "full", "thumbnail|medium|large", "Image size to use" )
+						, "post_type" => bw_skv( "attachment", "<i>post type</i>", "Post type" )
+						, "post_mime_type" => bw_skv( "image", "<i>post mime type</i>", "Post mime type" )
+						);
+	return( $syntax );
 }
  
  
