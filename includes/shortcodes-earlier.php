cb927259c5a889bda3068040f7ae471c7c422685
Herb Miller <herb@bobbingwide.com>
2020-08-27 21:08:59 +0100

    Issue #11 - update do_shortcode_earlier to be more like do_shortcode

--- includes/shortcodes-earlier.php
+++ includes/shortcodes-earlier.php
@@ -68,19 +68,37 @@
  * @return string Content with shortcodes expanded
  */
 if ( !function_exists( "do_shortcode_earlier" ) ) { 
-function do_shortcode_earlier($content) {
-	global $shortcode_tags;
 
-	if ( false === strpos( $content, '[' ) ) {
+
+	function do_shortcode_earlier( $content, $ignore_html = false ) {
+		global $shortcode_tags;
+
+		if ( false === strpos( $content, '[' ) ) {
+			return $content;
+		}
+
+		if ( empty( $shortcode_tags ) || ! is_array( $shortcode_tags ) ) {
+			return $content;
+		}
+
+		// Find all registered tag names in $content.
+		preg_match_all( '@\[([^<>&/\[\]\x00-\x20=]++)@', $content, $matches );
+		$tagnames = array_intersect( array_keys( $shortcode_tags ), $matches[1] );
+
+		if ( empty( $tagnames ) ) {
+			return $content;
+		}
+
+		$content = do_shortcodes_in_html_tags( $content, $ignore_html, $tagnames );
+
+		$pattern = get_shortcode_regex( $tagnames );
+		$content = preg_replace_callback( "/$pattern/", 'do_shortcode_tag_earlier', $content );
+
+		// Always restore square braces so we don't break things like <!--[if IE ]>.
+		$content = unescape_invalid_shortcodes( $content );
+
 		return $content;
 	}
-
-	if (empty($shortcode_tags) || !is_array($shortcode_tags))
-		return $content;
-
-	$pattern = get_shortcode_regex();
-	return preg_replace_callback( "/$pattern/s", 'do_shortcode_tag_earlier', $content );
-}
 }
 
 /**
